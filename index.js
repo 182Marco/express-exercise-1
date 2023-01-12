506b595bfa0624f7fcd13935a45aecdbb7cca4e6
182Marco <marcomilza@gmail.com>
2023-01-12 18:54:23 +0100

    string validators -> also enum

--- index.js
+++ index.js
@@ -8,34 +8,61 @@ mongoose
   .catch(er => console.er('error:', er))
 
 const courseSchema = new mongoose.Schema({
-  _id: {type: String},
-  name: String,
+  // _id: {type: String},
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 10,
+    match: /^by/i
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ['mobile', 'web', 'network']
+  },
   author: String,
   tags: [String],
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
-  price: Number,
+  price: {
+    type: String,
+    required: function() {
+      return this.isPublished
+    }},
 })
 
 const Course = mongoose.model('Courses', courseSchema)
 
+const createCourse = async () => {
+  const c = new Course({
+     name: 'by a name',
+     isPublished: false,
+     category: 'mobile'
+  })
+  const res = await c.save()
+  console.log('this is the creation res: ', res)
+}
+
+// createCourse()
+
 const getCourse = async () =>
   await Course.find({
-    isPublished: true,
+    name: /.*by.*/i,
   })
-    .or([{ price: { $gte: 15 } }, { name: { $regex: /.*by.*/i } }])
-    .sort('-price')
-    .select('name author price')
+    .select('name author price category')
 
 getCourse().then(r => console.log(r))
 
 
 const upDateCourse =  async (id) => {
-  const c = await Course.findById(id);
-  if(!c) return
-  c.author = 'other author'
-  const r = await c.save()
-  return r
+  const res = await Course.findByIdAndUpdate({_id: id}, {$set: { author: 'Mosh __bau'}}, {new: true})
+  console.log('this are the results of the direct update: ', res)
 }
 
-upDateCourse('5a6900fff467be65019a9001').then(r => console.log(r))
+const reomoveCourse =  async (id) => {
+  const res = await Course.findByIdAndRemove({_id: id}, {$set: { author: 'Mosh __bau'}}, {new: true})
+  console.log('this are the results of the direct update: ', res)
+}
+
+// upDateCourse('5a6900fff467be65019a9001').then(r => console.log(r))
